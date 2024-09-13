ba043a037f13186cbfa833caf94a7516820bd0b5
Aish-ika031 <aishikavishwakarma@gmail.com>
2024-09-13 20:19:35 +0530

    Found Maximum subarray sum

---
+++ Maximun Subarray Sum.py
@@ -0,0 +1,8 @@
+nums=list(map(int, input().split()))
+maxx=-999999
+for i in range(len(nums)):
+    curr_sum=0
+    for j in range(i,len(nums)):
+        curr_sum+=nums[j]
+        maxx=max(maxx,curr_sum)
+print("Maximum Subarray Sum:", maxx)
